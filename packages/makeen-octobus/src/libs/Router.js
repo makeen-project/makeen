76a18a11d024b8202cd6efc13fb21e9bde125c68
Victor Zamfir <zamfir.victor@gmail.com>
2017-06-29 23:55:47 +0300

    experimental octobus RPC

---
+++ packages/makeen-octobus/src/libs/Router.js
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+import { Message } from 'octobus.js';
+
+const router = Router();
+
+router.post('/rpc/:topic(.*)', async (req, res) => {
+  const topic = req.params && req.params.topic.replace(/\//g, '.');
+  if (!topic) {
+    return res.status(400).send({ error: 'Bad data!' });
+  }
+
+  const data = req.body;
+  const message = new Message({ topic, data });
+
+  try {
+    const result = await req.app.messageBus.send(message);
+    return res.json(result);
+  } catch (error) {
+    return res.status(500).send({ error });
+  }
+});
+
+export default router;
