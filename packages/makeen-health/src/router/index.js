b8dfad91b5e2b631f1442e9fd733468bf7f885b0
Victor Zamfir <zamfir.victor@gmail.com>
2017-06-30 12:17:33 +0300

    makeen-health displays uptime

--- packages/makeen-health/src/router/index.js
+++ packages/makeen-health/src/router/index.js
@@ -5,6 +5,7 @@ const router = Router();
 router.get('/health', (req, res) => {
   res.json({
     startTime: req.app.get('startTime'),
+    upTime: Date.now() - req.app.get('startTime'),
     memoryUsage: process.memoryUsage(),
   });
 });
