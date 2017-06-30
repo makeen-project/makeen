a6bd720bc61675b618c1b935ed0ad91860088b31
Victor Zamfir <zamfir.victor@gmail.com>
2017-06-30 12:58:28 +0300

    simpler server setup

--- packages/makeen-boilerplate/src/index.js
+++ packages/makeen-boilerplate/src/index.js
@@ -1,16 +1,26 @@
 /* eslint-disable no-console */
+import { Application } from 'makeen';
+import expressReactViews from 'express-react-views';
 import notifier from 'node-notifier';
 import chalk from 'chalk';
 import './env';
-import Server from './server';
+import Config from './config';
+import modules from './modules';
+import middlewares from './config/middlewares';
 
 const startTime = Date.now();
-const app = new Server();
+const app = new Application();
+
+app.set('views', `${__dirname}/views`);
+app.set('view engine', 'js');
+app.engine('jsx', expressReactViews.createEngine());
+app.middlewares.push(...middlewares);
+app.modules.add(modules);
 
 app
-  .listen(app.config.get('port'))
+  .listen(Config.get('port'))
   .then(() => {
-    const message = `Server started on port ${app.config.get(
+    const message = `Server started on port ${Config.get(
       'port',
     )} in ${Date.now() - startTime}ms!`;
 
