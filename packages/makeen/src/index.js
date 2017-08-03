62d9e69ae404918b6018229a13d454d4bdba204e
Victor Zamfir <zamfir.victor@gmail.com>
2017-08-03 15:56:48 +0300

    code cleanup

--- packages/makeen/src/index.js
+++ packages/makeen/src/index.js
@@ -1,8 +1,7 @@
 import Application from './libs/Application';
-import Config from './libs/Config';
 import * as helpers from './libs/helpers';
 import ModulesManager from './libs/ModulesManager';
 import Module from './libs/Module';
 import OrderedList from './libs/OrderedList';
 
-export { Application, Config, helpers, ModulesManager, Module, OrderedList };
+export { Application, helpers, ModulesManager, Module, OrderedList };
